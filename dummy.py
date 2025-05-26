6d10d01625b335ccb5f40f1d5f36861d5dbb0a0b
Sangjun Lim <118416669+IMLAB-sangjun@users.noreply.github.com>
2025-05-26 10:26:43 +0900

    Delete dummy.py

--- dummy.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-
-a = np.load('data/img_feature/rubber/dashboard_4_1_9137_features.npy')
-
-print(a.shape)
-
-b= np.load('data/img/rubber/dashboard_1_1_8816.npy')
-print(b.shape)
